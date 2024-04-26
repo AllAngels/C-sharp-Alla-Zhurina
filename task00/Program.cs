b4dcde64ad2671ae0b9d1e72805c1ca2ff43c396
Alla Zh <allagj7@yandex.ru>
2024-04-26 11:12:06 +0600

    1step+

--- task00/Program.cs
+++ task00/Program.cs
@@ -16,8 +16,7 @@ Console.ReadLine();
  
 static int SumNumbers(int M, int N)
 {
-    if (M == 0) return (N * (N + 1
-    )) / 2;            // Если M равно нулю
+    if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
     else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
     else if (M == N) return M;                       // Если M=N
     else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
